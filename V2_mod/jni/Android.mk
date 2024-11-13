56a874aecd2a928280b06eabab09cd6782005647
xNuclearSquirrel <aljoscha.soell@gmail.com>
2024-11-14 00:45:37 +0100

    Update Android.mk

--- V2_mod/jni/Android.mk
+++ V2_mod/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := O3_OSD_overlay
+LOCAL_MODULE    := O3_OSD_recording
 LOCAL_SRC_FILES := gs_lv_transcode_rec_omx_start.c MxDisplayPortDisplayPort_DrawScreen.c # Add all relevant hook files here
 
 
