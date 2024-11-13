359b934098e140c7f2307d4e63f2ad8906317108
xNuclearSquirrel <aljoscha.soell@gmail.com>
2024-11-14 00:48:27 +0100

    Add files via upload
    Mod for goggles V2 to record OSD

---
+++ Goggles_MOD/obj/local/armeabi-v7a/objs/O3_OSD_recording/MxDisplayPortDisplayPort_DrawScreen.o.d
@@ -0,0 +1,3 @@
+E:\Documents\gogglesV2\builds\working_v3format\obj/local/armeabi-v7a/objs/O3_OSD_recording/MxDisplayPortDisplayPort_DrawScreen.o: \
+  jni/MxDisplayPortDisplayPort_DrawScreen.c jni/ring_buffer.h
+jni/ring_buffer.h:
