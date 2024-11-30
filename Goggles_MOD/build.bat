cceb30c6f81084e191c4fc857326f43fb8c205e8
xNuclearSquirrel <aljoscha.soell@gmail.com>
2024-11-30 16:58:41 +0100

    Delete Goggles_MOD directory
    added new repo for the mod

--- Goggles_MOD/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set NDK_OUT=E:\Documents\gogglesV2\builds\working_v3format\obj
-set NDK_LIBS_OUT=E:\Documents\gogglesV2\builds\working_v3format\libs
-call ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=jni/Application.mk
